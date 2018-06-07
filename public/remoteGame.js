c927f1431ee09ceaef684c068827f14d672892bd
GitRafaelZamora <RafaelZamora.Me@gmail.com>
2018-06-07 11:28:43 -0700

    editing collectBox()

--- public/remoteGame.js
+++ public/remoteGame.js
@@ -5,7 +5,6 @@ var player;
 var enemies;
 var cursors;
 var platforms;
-var hitPlatform;
 var collector = false;
 var height = 560;
 var width = 1008;
@@ -121,33 +120,25 @@ function onNewPlayer( data ) {
 	}
 
 	enemies.push(new RemotePlayer(data.id, game, player, data.x, data.y));
-	for (var i = 0; i < enemies.length; i++) {
-		if (enemies[i].alive) {
-			enemies[i].update();
-			game.physics.arcade.collide(player, enemies[i]);
-		}
-	}
-
-	hitPlatform = game.physics.arcade.collide(player, layer);
 }
 
 function onMovePlayer( data ) {
 	var movePlayer = playerById(data.id);
 
 	if (!movePlayer) {
-		console.log('Player not Found: ', data.id);
+		console.log('onMovePlayer Player not Found: ', data.id);
 		return;
 	}
 
-	movePlayer.x = data.x;
-	movePlayer.y = data.y;
+	movePlayer.player.x = data.x;
+	movePlayer.player.y = data.y;
 }
 
 function onRemovePlayer( data ) {
 	var removePlayer = playerById(data.id);
 
 	if (!removePlayer) {
-		console.log('Player not found : ', data.id);
+		console.log('herhehre not found : ', data.id);
 		return;
 	}
 
@@ -157,18 +148,12 @@ function onRemovePlayer( data ) {
 }
 
 function collectBox(sprite, tile) {
-	console.log('Player has colelcted a box: ');
-	console.log(tile);
-	console.log(sprite);
 	if (!collector && tile.index !== -1) {
-		collector = true;
 		tile.alpha = 0.2;
 		tile.index = 0;
 		layer.dirty = true;
+		collector = true;
 	}
-	console.log('this');
-	console.log(this);
-	// socket.emit('collect box', {tile: this});
 	return false;
 }
 
@@ -183,6 +168,13 @@ function depositBox(sprite, tile) {
 }
 
 function update() {
+	for (var i = 0; i < enemies.length; i++) {
+		if (enemies[i].alive) {
+			enemies[i].update();
+			game.physics.arcade.collide(player, enemies[i].player);
+		}
+	}
+	var hitPlatform = game.physics.arcade.collide(player, layer);
 	player.body.velocity.x = 0;
     if (cursors.right.isDown) {
         player.body.velocity.x = 150;
@@ -197,18 +189,11 @@ function update() {
     	player.frame = 4;
     }
     // Jump Mechanics
-    if (cursors.up.isDown && 
-    	hitPlatform && 
-    	player.body.blocked.down) 
-    {
+    if (cursors.up.isDown && hitPlatform && player.body.blocked.down) {
         player.body.velocity.y = -380;
     }
 
-    socket.emit('move player', {
-    	id: player.id, 
-    	x: player.x, 
-    	y: player.y
-    });
+    socket.emit('move player', {id: player.id , x: player.x, y: player.y});
 }
 
 // Find player by ID
@@ -221,12 +206,3 @@ function playerById ( id ) {
 
   return false
 }
-
-
-
-
-
-
-
-
-
